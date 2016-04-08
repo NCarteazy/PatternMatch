4c370dd46a4ef49bf9701e6ca9c6bd20ee09ce9b
NCarteazy <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-04-08 22:05:11 +0000

    Looks Good v2

--- split-match.js
+++ split-match.js
@@ -15,24 +15,23 @@ var PatternMatch = function () {
 util.inherits(PatternMatch, Transform);
 
 PatternMatch.prototype._transform = function (chunk, encoding, getNextChunk) {
-	console.log("Transform");
 	var data = chunk.toString();
 	if(this._lastLineData) data = this._lastLineData + data;
-	console.log(program.pattern);	
-	console.log(program.pattern2);
+		
 	var lines = data.split(program.pattern);
 	var lines2 = [];
 	var lines3 = [];
+
 	lines.forEach(function (element, index, array) {
 		lines2.push(element.split(program.pattern2).forEach(function (element, index, array) {
 				lines3.push(element);
 			})) 
-		console.log(element);
 	});
 	this._lastLineData = lines.splice(lines2.length-1,1)[0];
-
-	console.log(data)
 	lines.forEach(this.push.bind(this));
+	console.log("---------------------INPUT----------------------------");
+	console.log(data);
+	console.log("---------------------OUTPUT---------------------------");
 	console.log(lines3);
 };
 
@@ -44,8 +43,6 @@ program
 	.option('-p, --pattern <pattern>', 'Input Pattern such as . ,')
 	.option('-s, --pattern2 <pattern2>', 'Input Pattern2 such as . ,')
 	.parse(process.argv)
-console.log(program.pattern);
-console.log(program.pattern2);
 var inputStream = fileSystem.createReadStream( './input-sensor.txt' );
 var patternStream = inputStream.pipe( new PatternMatch());
 
